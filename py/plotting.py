adf4f172dc164cfdc74423ea90bc76faccc1d6f9
Sebastian Hoof <s.hoof.physics@gmail.com>
2023-07-20 16:14:53 +0100

    Added Matt's pickled exclusion limits for comparison.

--- py/plotting.py
+++ py/plotting.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 plt.rc('text', usetex=True)
-plt.rc('text.latex', preamble=r'\usepackage{lmodern}\usepackage[T1]{fontenc}\usepackage{amsmath}\usepackage{amssymb}\usepackage{siunitx}\DeclareSIUnit\year{yr}\usepackage[version=4]{mhchem}')
-plt.rc('pgf', texsystem='pdflatex', preamble=r'\usepackage{lmodern}\usepackage[T1]{fontenc}\DeclareUnicodeCharacter{2212}{-}\usepackage{amsmath}\usepackage{amssymb}\usepackage{siunitx}\DeclareSIUnit\year{yr}\usepackage[version=4]{mhchem}')
+#plt.rc('text.latex', preamble=r'\usepackage{lmodern}\usepackage[T1]{fontenc}\usepackage{amsmath}\usepackage{amssymb}\usepackage{siunitx}\DeclareSIUnit\year{yr}\usepackage[version=4]{mhchem}')
+#plt.rc('pgf', texsystem='pdflatex', preamble=r'\usepackage{lmodern}\usepackage[T1]{fontenc}\DeclareUnicodeCharacter{2212}{-}\usepackage{amsmath}\usepackage{amssymb}\usepackage{siunitx}\DeclareSIUnit\year{yr}\usepackage[version=4]{mhchem}')
+plt.rc('text.latex', preamble=r'\usepackage{newtxtext}\usepackage{newtxmath}\usepackage{amsmath}\usepackage{amssymb}\usepackage{siunitx}\DeclareSIUnit\year{yr}\usepackage[version=4]{mhchem}')
+plt.rc('pgf', texsystem='pdflatex', preamble=r'\usepackage{newtxtext}\usepackage{newtxmath}\usepackage{amsmath}\usepackage{amssymb}\usepackage{siunitx}\DeclareSIUnit\year{yr}\usepackage[version=4]{mhchem}')
 plt.rc('font', **{'family':'serif','size':10})
 plt.rc('axes', labelsize=10)
 plt.rc('xtick', **{'labelsize':8, 'major.pad':4})
@@ -22,6 +24,7 @@ plt.rcParams["legend.fancybox"] = False
 plt.rcParams["legend.frameon"] = False
 
 jcap_width = 6 # inches
+mnras_width = 3.38 # inches
 golden_ratio = 0.5*(1.0 + np.sqrt(5.0))
 
 def set_style(gs=10, lts=10, lfs=8, lbls=10, tls=10):
@@ -92,10 +95,10 @@ def sci_format(x):
     return '{:.2f} \\times 10^{{ {:d} }}'.format(sign*lead, exp)
 
 ## Specific plotting functions
-def plot_m_a(m, a, label='', bfit=None):
+def plot_m_a(m, a, label='', bfit=None, xlims=[5,25], ylims=[0,1]):
     phi = np.linspace(0, 2*np.pi, 100)
-    plt.figure(figsize=(3.375,3))
-    plt.plot(m, a, 'o', ms=1, c='0.6', rasterized=True)
+    plt.figure(figsize=(mnras_width, 3))
+    plt.plot(m, a, 'o', ms=1, c='0.6', rasterized=True, label=r"Samples")
     m_mean, a_mean = np.mean(m), np.mean(a)
     m_std, a_std = np.std(m), np.std(a)
     cov = np.cov((m,a))
@@ -104,15 +107,17 @@ def plot_m_a(m, a, label='', bfit=None):
         plt.plot(bfit[0], bfit[1], ms=5, c='k', marker='*')
     print("m = {:.2f} +/- {:.2f}, a = {:.2f} +/- {:.2f}".format(m_mean, m_std, a_mean, a_std))
     xp = np.array([2*c@np.diag(np.sqrt(l))@v for v in [np.array([np.cos(x), np.sin(x)]).T for x in phi]])
-    plt.plot(m_mean + 2*m_std*np.cos(phi), a_mean + 2*a_std*np.sin(phi), 'b--')
-    plt.plot(m_mean + xp[:,0], a_mean + xp[:,1], 'r-')
-    plt.plot(m_mean+2*m_std*np.array([1,1,-1,-1,1]), a_mean+2*a_std*np.array([1,-1,-1,1,1]), 'k:')
-    plt.ylim([0,1])
-    plt.xlim([5,25])
-    plt.xlabel(r"BH mass [$M_\odot$]")
-    plt.ylabel(r"BH spin $a_\ast$")
-    plt.text(24, 0.1, label, ha='right', va='center')
+    plt.plot(m_mean + 2*m_std*np.cos(phi), a_mean + 2*a_std*np.sin(phi), 'b--', label=r"Uncorrelated Gaussian")
+    plt.plot(m_mean + xp[:,0], a_mean + xp[:,1], 'r-', label=r"Full Gaussian")
+    plt.plot(m_mean+2*m_std*np.array([1,1,-1,-1,1]), a_mean+2*a_std*np.array([1,-1,-1,1,1]), 'k:', label=r"`Box method'")
+    plt.ylim(ylims)
+    plt.xlim(xlims)
+    plt.xlabel(r"Black hole mass [$M_\odot$]")
+    plt.ylabel(r"Black hole spin $a_\ast$")
+    plt.legend(frameon=False)
     if label !='':
-        plt.tight_layout(pad=0.25)
+        plt.tight_layout()
         plt.savefig("figures/"+label+".pdf", backend='pgf')
+    else:
+        plt.text(0.95*xlims[1], 0.1, label, ha='right', va='center')
     plt.show()
